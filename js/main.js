75a421adcb2f4d8d78f10929cb408581551be108
etgertc <104611103+etgertc@users.noreply.github.com>
2022-04-28 21:13:12 -0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,56 @@
+const kun = prompt("n ; a ; d ; S");
+
+switch (kun) {
+    case s:
+        // document.write("<div class='kun'>Dushanba</div>")
+
+
+        const a = prompt("a1 ni kiriting")
+        const n = prompt("n-ni kiriting")
+        const d = prompt("d ni kiriting")
+
+        let ber = a * 2
+        let nn = n - 1
+        let gg = nn * d
+        let fgh = gg + ber
+        let fhh = fgh * n
+        let dvd = fhh / 2
+        alert(dvd)
+        document.write(`<p style=" font-size: 100px; color: black; font-weight: 900;">${dvd}</p>`)
+
+        break;
+    case 2: alert("Sheyshanba")
+        break;
+    case 3:
+        alert("Chorshanba")
+        break;
+    case 4:
+        alert("Payshanba")
+        break;
+    case 5:
+        alert("Juma")
+        break;
+    case 6:
+        alert("sahnba")
+        break;
+    case 7:
+        alert("yakshanba")
+        break;
+    default: alert("Bunday kun mavjud emas")
+        break;
+}
+console.log(typeof (kun))
+
+
+// const a = prompt("a1 ni kiriting")
+// const n = prompt("n-ni kiriting")
+// const d = prompt("d ni kiriting")
+
+// let ber = a * 2
+// let nn = n - 1
+// let gg = nn * d
+// let fgh = gg + ber
+// let fhh = fgh * n
+// let dvd = fhh / 2
+// alert(dvd)
+// document.write(`<p style=" font-size: 100px; color: black; font-weight: 900;">${dvd}</p>`)
